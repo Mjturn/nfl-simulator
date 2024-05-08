4f62833c8b1ed87a9679e60f432042a25b51996f
Mjturn <Mjturn@outlook.com>
2024-05-08 15:36:32 -0400

    construct array of teams

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    char* teams[] = {
+        "San Francisco 49ers",
+        "Chicago Bears",
+        "Cincinnati Bengals",
+        "Buffalo Bills",
+        "Denver Broncos",
+        "Cleveland Browns",
+        "Tampa Bay Buccaneers",
+        "Arizona Cardinals",
+        "Los Angeles Chargers",
+        "Kansas City Chiefs",
+        "Indianapolis Colts",
+        "Washington Commanders",
+        "Dallas Cowboys",
+        "Miami Dolphins",
+        "Philadelphia Eagles",
+        "Atlanta Falcons",
+        "New York Giants",
+        "Jacksonville Jaguars",
+        "New York Jets",
+        "Detroit Lions",
+        "Green Bay Packers",
+        "Carolina Panthers",
+        "New England Patriots",
+        "Las Vegas Raiders",
+        "Los Angeles Rams",
+        "Baltimore Ravens",
+        "New Orleans Saints",
+        "Seattle Seahawks",
+        "Pittsburgh Steelers",
+        "Houston Texans",
+        "Tennessee Titans",
+        "Minnesota Vikings"
+    };
+
+    return 0;
+}
