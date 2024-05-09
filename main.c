b9f1d5d74edfdc09d97a63f7dd338e02d65d4ed2
Mjturn <Mjturn@outlook.com>
2024-05-09 15:23:40 -0400

    generate 17 week schedule

--- main.c
+++ main.c
@@ -1,4 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void shuffle_teams(char** teams, const int TEAMS_LENGTH) {
+    for(int i = 0; i < TEAMS_LENGTH; i++) {
+        int j = rand() % TEAMS_LENGTH;
+        char* temp = teams[i];
+        teams[i] = teams[j];
+        teams[j] = temp;
+    }
+}
+
+void generate_schedule(char** teams, const int TEAMS_LENGTH) {
+    for(int week = 1; week <= 17; week++) {
+        printf("Week %d:\n", week);
+        shuffle_teams(teams, TEAMS_LENGTH);
+        int teams_scheduled[TEAMS_LENGTH];
+
+        for(int i = 0; i < TEAMS_LENGTH; i++) {
+            teams_scheduled[i] = 0;
+        }
+
+        for(int i = 0; i < TEAMS_LENGTH; i++) {
+            int j = rand() % TEAMS_LENGTH;
+
+            if(teams_scheduled[i]) {
+                continue;
+            }
+
+            while(i == j || teams_scheduled[j]) {
+                j = rand() % TEAMS_LENGTH;
+            }
+
+            printf("%s vs. %s\n", teams[i], teams[j]);
+            
+            teams_scheduled[i] = 1;
+            teams_scheduled[j] = 1;
+        }
+
+        printf("\n");
+    }
+}
 
 int main() {
     char* teams[] = {
@@ -35,6 +77,10 @@ int main() {
         "Tennessee Titans",
         "Minnesota Vikings"
     };
+
+    srand(time(NULL));
+    const int TEAMS_LENGTH = sizeof(teams) / sizeof(teams[0]);
+    generate_schedule(teams, TEAMS_LENGTH);
 
     return 0;
 }
